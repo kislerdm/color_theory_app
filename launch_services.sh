09750aed6549f7be7965e3bca7d0414e7c8f1e14
Dmitry Kisler <admin@dkisler.de>
2019-05-25 19:04:01 +0200

    docker adjustment

--- launch_services.sh
+++ launch_services.sh
@@ -8,27 +8,38 @@
 IMAGE=color_theory_app
 
 # host ports
-PORT_BE0=4500
-PORT_BE=${PORT_BE0}
+PORT_BE_ML0=4500
+PORT_BE_ML=${PORT_BE_ML0}
+
+PORT_BE_BASE0=4499
+PORT_BE_BASE=${PORT_BE_BASE0}
 
 PORT_FE0=3000
 PORT_FE=${PORT_FE0}
 
 # backend API end-point
-BACKEND_API0=http://localhost:${PORT_BE}
-BACKEND_API=${BACKEND_API0}
+BACKEND_API_ML0=http://localhost:${PORT_BE_ML}
+BACKEND_API_ML=${BACKEND_API_ML0}
+
+BACKEND_API_BASE0=http://localhost:${PORT_BE_BASE}
+BACKEND_API_BASE=${BACKEND_API_BASE0}
+
 
 # launch backend API
 usage () {
     cat <<HELP_USAGE
 
-    $0  -b [--port_be] -f [--port_fe] -u [--url]
+    $0  --port_be_base --port_be_ml --port_fe --urlbase --urlml
 
-   -b [--port_be] Port to expose backend API end-point [default 4500].
+   --port_be_base Port to expose backend BASE API end-point [default 4499].
 
-   -f [--port_fe] Port to serve frontend app [default 3000].
+   --port_be_ml Port to expose backend ML API end-point [default 4500].
 
-   -u [--url] URL to access backend API by the frontend app [delafut http://localhost:${PORT_BE}]
+   --port_fe Port to serve frontend app [default 3000].
+
+   --urlbase URL to access backend BASE API by the frontend app [delafut ${BACKEND_API_BASE0}]
+
+   --urlml URL to access backend ML API by the frontend app [delafut ${PORT_BE_ML0}]
 
 HELP_USAGE
 }
@@ -40,14 +51,20 @@ msg () {
 # parse arguments
 while [ "$1" != "" ]; do
     case $1 in
-        -b | --port_be )        shift
-                                PORT_BE=$1
+        --port_be_base )        shift
+                                PORT_BE_BASE=$1
                                 ;;
-        -f | --port_fe )        shift
+        --port_be_ml )          shift
+                                PORT_BE_ML=$1
+                                ;;
+        --port_fe )             shift
                                 PORT_FE=$1
                                 ;;
-        -u | --url )            shift
-                                BACKEND_API=$1
+        --urlbase )             shift
+                                BACKEND_API_BASE=$1
+                                ;;
+        --urlml )               shift
+                                BACKEND_API_ML=$1
                                 ;;
         -h | --help )           usage
                                 exit
@@ -59,15 +76,17 @@ while [ "$1" != "" ]; do
 done
 
 # check the API URL
-if [[ ( ! ${PORT_BE} -eq ${PORT_BE0} ) &&
+if [[ ( ! ${PORT_BE_ML} -eq ${PORT_BE_ML0} ) &&
      ( "${BACKEND_API}" == "${BACKEND_API0}" ) ]]; then
-       BACKEND_API=http://localhost:${PORT_BE}
+       BACKEND_API=http://localhost:${PORT_BE_ML}
 fi
 
 # export variables
-export PORT_BE=${PORT_BE}
+export PORT_BE_BASE=${PORT_BE_BASE}
+export PORT_BE_ML=${PORT_BE_ML}
 export PORT_FE=${PORT_FE}
-export BACKEND_API=${BACKEND_API}
+export BACKEND_API_BASE=${BACKEND_API_BASE}
+export BACKEND_API_ML=${BACKEND_API_ML}
 
 # build and run microservices containers
 
