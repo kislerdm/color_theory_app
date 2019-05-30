b7e693df09965b42473576ce92ec34f05f905be2
Dmitry Kisler <admin@dkisler.de>
2019-05-30 21:36:25 +0200

    base app fronted+dockerfile added

---
+++ frontend/app-base/src/index.js
@@ -0,0 +1,6 @@
+import './index.css';
+import './features/get_color_code.js';
+import './features/get_color_name.js';
+import * as serviceWorker from './serviceWorker';
+
+serviceWorker.unregister();
